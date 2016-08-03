f7cd02fa560d02c9c18f6eaa57288ca7b067c1ca
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-08-03 19:11:35 +0200

    Refactoring

--- src/test/java/it/besil/tutorials/jwebsample/AllTests.java
+++ src/test/java/it/besil/tutorials/jwebsample/AllTests.java
@@ -1,6 +1,7 @@
 package it.besil.tutorials.jwebsample;
 
 import it.besil.tutorials.jwebsample.echo.EchoTest;
+import it.besil.tutorials.jwebsample.filter.FilterTest;
 import it.besil.tutorials.jwebsample.helloworld.HelloWorldTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,7 +10,7 @@ import org.junit.runners.Suite;
  * Created by besil on 03/08/2016.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({EchoTest.class, HelloWorldTest.class})
+@Suite.SuiteClasses({EchoTest.class, HelloWorldTest.class, FilterTest.class})
 public class AllTests {
 
 }
--- src/test/java/it/besil/tutorials/jwebsample/filter/FilterTest.java
+++ src/test/java/it/besil/tutorials/jwebsample/filter/FilterTest.java
@@ -1,13 +1,31 @@
 package it.besil.tutorials.jwebsample.filter;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import it.besil.jweb.testing.AbstractBehaviouralTest;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by besil on 03/08/2016.
  */
-public class FilterTest {
-    @Test
-    public void filterTest() {
+public class FilterTest extends AbstractBehaviouralTest {
+    @Before
+    public void prepare() {
+        addApp(new FilterApp());
+    }
 
+    @Test
+    public void filterTest() throws UnirestException {
+        HttpResponse<JsonNode> home = Unirest.get(getUrl("/home")).asJson();
+        Assert.assertEquals("Not authorized", home.getBody().getObject().get("error"));
+
+        home = Unirest.get(getUrl("/home"))
+                .queryString("user", "admin")
+                .asJson();
+        Assert.assertEquals("secret data here", home.getBody().getObject().get("secret"));
     }
 }
